a5255e7f7faf81a807ac2a2ab02cf2f69fbaf54f
Vladymyr Shevchuk <dosandkv@gmail.com>
2021-06-04 22:58:49 +0300

    init commit

---
+++ webpack.dev.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  entry: {
+    app: path.join(__dirname, './src/entry-point.js')
+  },
+  output: {
+    publicPath: '/',
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, './dist'),
+    chunkFilename: '[name]-[id].js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    compress: true,
+    port: 9000,
+  },
+};
